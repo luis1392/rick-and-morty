bfcc08e49a7fe4c4a7e84b8a6ca9bbe81a601e1d
luis1392 <luis.enri1392@gmail.com>
2020-09-30 14:19:37 -0500

    Updates

---
+++ precache-manifest.23662ee47a141195dd5d8babdfa258e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4790e69c403379f3f25565e35a4628",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "d7df0a84e7c09f43ec0b",
+    "url": "/rick-and-morty/static/css/2.831443e2.chunk.css"
+  },
+  {
+    "revision": "538ddffcf654fd76acd5",
+    "url": "/rick-and-morty/static/css/main.3f0c851a.chunk.css"
+  },
+  {
+    "revision": "d7df0a84e7c09f43ec0b",
+    "url": "/rick-and-morty/static/js/2.50eed5da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-and-morty/static/js/2.50eed5da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538ddffcf654fd76acd5",
+    "url": "/rick-and-morty/static/js/main.470c05e9.chunk.js"
+  },
+  {
+    "revision": "8ec831075791f93706ea",
+    "url": "/rick-and-morty/static/js/runtime-main.2b22dfe8.js"
+  }
+]);
